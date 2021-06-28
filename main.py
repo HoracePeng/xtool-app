bae6ae361764f6cc542977d8435a3bf499b3a894
China.peng <china.peng@outlook.com>
2021-06-28 17:33:43 +0800

    complete add friend component.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
 import platform
 import re
+import time
 from enum import Enum
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-from PyQt5.QtCore import *
 from xtool import Ui_MainWindow
+import pyautogui
 
 if platform.system() == "Windows":
     import ctypes
@@ -14,9 +15,9 @@ if platform.system() == "Windows":
 
 # 号码状态
 class PhoneState(Enum):
-    Waiting = 1,
-    Succeed = 2,
-    Failed = 3
+    Waiting = 1,  # 等待添加
+    Succeed = 2,  # 添加成功
+    Failed = 3,  # 添加失败
 
     def __str__(self):
         if self == PhoneState.Waiting:
@@ -73,11 +74,84 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
                     state_item.setBackground(QColor(0, 255, 0))
 
                 self.phoneTable.setItem(row, 2, state_item)
-        # QMessageBox.warning(self, "标题", "警告消息正文")
 
     def start_invite(self):
-        QMessageBox.information(self, "执行", "点击了执行按钮")
+        # 第一步切换到企业微信组织架构页面
+        select_location = pyautogui.locateCenterOnScreen('./stepPic/select.png', confidence=0.9)
+        if not select_location:
+            un_select_location = pyautogui.locateCenterOnScreen('./stepPic/un_select.png', confidence=0.9)
+            if not un_select_location:
+                QMessageBox.warning(self, "错误", "未找到组织架构这一菜单栏")
+                return
+            # 进入组织架构
+            pyautogui.click(un_select_location)
 
+        new_customer_selected = pyautogui.locateCenterOnScreen('./stepPic/new_customer_selected.png', confidence=0.9)
+        if not new_customer_selected:
+            new_customer_un_select = pyautogui.locateCenterOnScreen('./stepPic/new_customer_un_select.png',
+                                                                    confidence=0.9)
+            if not new_customer_un_select:
+                QMessageBox.warning(self, "错误", "未找到<新的客户>")
+                return
+            pyautogui.click(new_customer_un_select)
+
+        # 开始循环添加用户
+        for [row, friend_info] in enumerate(self.friend_infos):
+            if friend_info["state"] != PhoneState.Waiting:
+                continue
+            try:
+                state = self.add_friend(friend_info["phone"])
+            except RuntimeError as e:
+                QMessageBox.warning(self, "错误", str(e))
+                return
+            state_item = QTableWidgetItem(str(state))
+            if friend_info["state"] == PhoneState.Failed:
+                state_item.setBackground(QColor(255, 0, 0))
+            elif friend_info["state"] == PhoneState.Succeed:
+                state_item.setBackground(QColor(0, 255, 0))
+            self.phoneTable.setItem(row, 2, state_item)
+
+            # 间隔指定秒数
+            time.sleep(self.delayTimeBox.Value())
+
+        QMessageBox.information(self, "信息", "完成添加任务")
+
+    @staticmethod
+    def add_friend(phone):
+        add_icon = pyautogui.locateCenterOnScreen('./stepPic/add_icon.png', confidence=0.9)
+        if not add_icon:
+            raise RuntimeError("未找到添加按钮")
+        pyautogui.click(add_icon)
+        input_tel_icon = pyautogui.locateCenterOnScreen('./stepPic/input_tel_icon.png', confidence=0.9)
+        if not input_tel_icon:
+            raise RuntimeError("未找到输入栏")
+        pyautogui.click(input_tel_icon)
+        pyautogui.typewrite(phone, interval=0.25)
+        time.sleep(1)
+        # 查找用户
+        search_result = pyautogui.locateCenterOnScreen('./stepPic/search_result.png', confidence=0.9)
+        if not search_result:
+            not_found_user = pyautogui.locateCenterOnScreen('./stepPic/not_found_user.png', confidence=0.9)
+            if not_found_user:
+                confirm_not_found_user = pyautogui.locateCenterOnScreen('./stepPic/confirm_not_user.png', confidence=0.9)
+                pyautogui.click(confirm_not_found_user)
+                return PhoneState.Failed
+        # 处理已添加 和 未添加
+        add = pyautogui.locateCenterOnScreen('./stepPic/add.png')
+        if not add:
+            has_added = pyautogui.locateCenterOnScreen('./stepPic/has_added.png')
+            if has_added:
+                return PhoneState.Succeed
+            else:
+                raise RuntimeError("未知错误")
+        pyautogui.click(add)
+        time.sleep(2)
+        send = pyautogui.locateCenterOnScreen('./stepPic/send.png', confidence=0.9)
+        if not send:
+            raise RuntimeError("未找到发送按钮")
+        pyautogui.click(send)
+        time.sleep(2)
+        return PhoneState.Succeed
 
 def main():
     # 初始化app
